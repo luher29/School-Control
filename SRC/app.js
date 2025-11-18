8cc4545915d618348105e3ebb4297678d7b2412d
Lupita <luher2912@gmail.com>
2025-11-18 16:00:06 -0600

    Cambio de src en app

--- SRC/app.js
+++ SRC/app.js
@@ -14,7 +14,7 @@ const app = express();
 //Settings
 app.set("port",process.env.PORT||3000);
 app.set("view engine","ejs");
-app.set("views","./SRC/views");
+app.set("views","./src/views");
 
 
 //Middlewares
